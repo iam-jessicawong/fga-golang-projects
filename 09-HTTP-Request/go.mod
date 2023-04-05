e8cc4fb1dcae2c91c11bdf2a75db75e140e15d6c
Jessica Wong <wongjessica124@gmail.com>
2023-04-05 14:23:21 +0700

    add assignment 9 go vendoring and set up simple http request

---
+++ 09-HTTP-Request/go.mod
@@ -0,0 +1,5 @@
+module 09-http-request
+
+go 1.20
+
+require github.com/novalagung/gubrak/v2 v2.0.1 // indirect
---
+++ 09-HTTP-Request/handlers.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	h8HelperRand "github.com/novalagung/gubrak/v2"
+)
+
+func PostRequest() {
+	// initialize request data
+	data := Data{Water: h8HelperRand.RandomInt(1, 100), Wind: h8HelperRand.RandomInt(1, 100)}
+	apiURL := "https://jsonplaceholder.typicode.com/posts"
+
+	reqJson, err := json.Marshal(data) // convert request data object to json
+	client := &http.Client{}
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	// make post request ot api url
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(reqJson))
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	// check water and wind status based on request data
+	statusWater := CheckWaterLevel(data.Water)
+	statusWind := CheckWindLevel(data.Wind)
+	res, err := client.Do(req) //send request to api
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer res.Body.Close()
+
+	// read response body
+	body, err := io.ReadAll(res.Body)
+	// err = json.NewDecoder(res.Body).Decode(&result)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	// print response result
+	fmt.Println(string(body))
+	fmt.Println("status water:", statusWater)
+	fmt.Println("status wind:", statusWind)
+}
